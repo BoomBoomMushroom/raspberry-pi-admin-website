53e34889e2ae61f281d9fafe4bc8059f1dc3aeaf
BoomBoomMushroom <dillioncat7@gmail.com>
2024-09-11 20:25:00 -0400

    comment working path print

--- start.py
+++ start.py
@@ -1,8 +1,9 @@
 import os
 
 # Get working directory and print it
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(f"Working path: {dir_path}")
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+#print(f"Working path: {dir_path}")
+print("Do i need to print working path?")
 
 # Pull any updates!
 os.system("git pull")
