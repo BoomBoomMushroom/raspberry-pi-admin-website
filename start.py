5f24d465ec49d58a5fda4a91e93b75f35f715b05
BoomBoomMushroom <dillioncat7@gmail.com>
2024-09-11 20:24:04 -0400

    do git pull

--- start.py
+++ start.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+import os
+
+# Get working directory and print it
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(f"Working path: {dir_path}")
+
+# Pull any updates!
+os.system("git pull")
