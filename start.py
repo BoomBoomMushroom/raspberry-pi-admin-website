f210986980929b045b3c845b3faabf1b4cb36f2e
BoomBoomMushroom <dillioncat7@gmail.com>
2024-09-11 21:14:12 -0400

    changed how handling git pull

--- start.py
+++ start.py
@@ -1,21 +1,22 @@
 import os
 import subprocess
 import sys
-import threading
 
-# Pull any updates!
-pullCommand = "git pull"
-noUpdatesOutput = "Already up to date."
+try:
+    # Pull any updates!
+    pullCommand = "git pull"
+    noUpdatesOutput = "Already up to date."
 
-pullOutput = subprocess.check_output(pullCommand, shell=True, text=True)
-firstLineOfPullOutput = pullOutput.split("\n")[0]
+    pullOutput = subprocess.check_output(pullCommand, shell=True, text=True)
+    firstLineOfPullOutput = pullOutput.split("\n")[0]
 
-didUpdate = firstLineOfPullOutput != noUpdatesOutput
+    didUpdate = firstLineOfPullOutput != noUpdatesOutput
 
-if didUpdate:
-    print("Restarting Raspberry Pi in order to make sure changes are applied!")
-    #os.system("sudo reboot")
-    #exit()
+    if didUpdate:
+        print("Updated code!")
+        #print("Restarting Raspberry Pi in order to make sure changes are applied!")
+except:
+    pass
 
 print("Everything is up to date! Now entering main program!")
 os.system("python main.py")
