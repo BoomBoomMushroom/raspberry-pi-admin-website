4c118e21343822999bea12071732dadbc56d4a69
BoomBoomMushroom <dillioncat7@gmail.com>
2024-09-11 21:54:23 -0400

    Major commit, checking updates and relaunching m,any scirpts

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-import os
+import subprocess
+import threading
+import time
+
+def closeScriptAndLaunchAnother(newScript="start.py"):
+    print(f"Closing & Launching {newScript}")
+    time.sleep(1)
+    subprocess.Popen(["python", newScript])
+    exit()
+
+
+def checkForUpdates():
+    commitDiffCommand = "git rev-list --count --left-right @{u}...HEAD"
+    pullOutput = subprocess.check_output(commitDiffCommand, shell=True, text=True)
+    commitsBehind = int(pullOutput.split("\t")[0])
+    
+    print(f"Checking if we are behind... {pullOutput}")
+    
+    if commitsBehind > 0:
+        print(f"We are {commitsBehind} commits behind!")
+        closeScriptAndLaunchAnother("start.py")
+        #exit()
+
+    threading.Timer(1, checkForUpdates).start()
+
+checkForUpdates()
 
 print("Hello, World! Main.py here!")
--- start.py
+++ start.py
@@ -1,22 +1,38 @@
 import os
+import subprocess
+import time
+
+
+def closeScriptAndLaunchAnother(newScript="start.py"):
+    print(f"Closing & Launching {newScript}")
+    time.sleep(1)
+    subprocess.Popen(["python", newScript])
+    exit()
 
 # Pull any updates!
 pullCommand = "git pull"
-os.system(pullCommand)
+#os.system(pullCommand)
 
-"""
 noUpdatesOutput = "Already up to date."
 
-pullOutput = subprocess.check_output(pullCommand, shell=True, text=True)
+try:
+    pullOutput = subprocess.check_output(pullCommand, shell=True, text=True)
+except:
+    pullOutput = noUpdatesOutput + "\n"
+
 firstLineOfPullOutput = pullOutput.split("\n")[0]
 
 didUpdate = firstLineOfPullOutput != noUpdatesOutput
 
 if didUpdate:
-    print("Restarting Raspberry Pi in order to make sure changes are applied!")
-    os.system("sudo reboot")
-    exit()
-"""
+    print("Not up to date, restarting")
+    os.system(pullCommand)
+    #print("Restarting Raspberry Pi in order to make sure changes are applied!")
+    #os.system("sudo reboot")
+    #exit()
+    closeScriptAndLaunchAnother("start.py")
+    
 
 print("Everything is up to date! Now entering main program!")
-os.system("python main.py")
+#os.system("python main.py")
+closeScriptAndLaunchAnother("main.py")
