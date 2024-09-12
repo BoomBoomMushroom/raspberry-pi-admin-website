e892956d6011dbd7dbd71014edec0e1057b5bc75
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2024-09-12 11:22:43 -0400

    Added testing webserver

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import time
+import flask
+from flask_cors import CORS
+import json
 
-print("Hello, World! Main.py here!")
-# to admin@piserver1
+print("Main.py starting the webserver!")
 
-time.sleep(3)
+def responseMake(r):
+    try:
+        json.dumps(r)
+    except:
+        pass
+    resp = flask.Response(json.dumps(r))
+    resp.headers.add('Access-Control-Allow-Origin', "*")
+    return resp
 
-print("Main.py out!")
+app = flask.Flask(__name__)
+CORS(app,
+     origins='*',
+     headers=['Content-Type', 'Authorization'],
+     expose_headers='Authorization')
+
+@app.route("/")
+def home():
+    return responseMake("Website is working, this is /"), 200
+
+
+app.run(host="0.0.0.0", port=7777)
